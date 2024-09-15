d3775796c03f4f1e9888f5a91772b82645ddb041
kingstar <admin@baks.dev>
2024-09-16 01:53:18 +0300

    Baks Development

---
+++ Resources/config/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+use BaksDev\Reference\Cars\BaksDevReferenceCarsBundle;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return static function (RoutingConfigurator $routes) {
+
+    $MODULE = BaksDevReferenceCarsBundle::PATH;
+
+    $routes->import(
+        $MODULE.'Controller',
+        'attribute',
+        false,
+        $MODULE.implode(DIRECTORY_SEPARATOR, ['Controller', '**', '*Test.php'])
+    )
+        ->prefix(\BaksDev\Core\Type\Locale\Locale::routes())
+        ->namePrefix('reference-cars:');
+};
