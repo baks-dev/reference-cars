a3746557b00754f2bb318baf097586ea66331a1b
kingstar <admin@baks.dev>
2025-01-17 01:15:15 +0300

    Baks Development

--- Resources/config/messenger.php
+++ Resources/config/messenger.php
@@ -33,7 +33,7 @@ return static function(FrameworkConfig $framework) {
 
     $messenger
         ->transport('reference-cars')
-        ->dsn('redis://%env(REDIS_PASSWORD)%@%env(REDIS_HOST)%:%env(REDIS_PORT)%?dbindex=%env(REDIS_TABLE)&auto_setup=true')
+        ->dsn('redis://%env(REDIS_PASSWORD)%@%env(REDIS_HOST)%:%env(REDIS_PORT)%?dbindex=%env(REDIS_TABLE)%&auto_setup=true')
         ->options(['stream' => 'reference-cars'])
         ->failureTransport('failed-reference-cars')
         ->retryStrategy()
